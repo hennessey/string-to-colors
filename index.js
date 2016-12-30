9deb58ac9778e3d2a1d386d5d8f8afb337b5a956
John Hennessey <jghennes@gmail.com>
2016-12-29 20:28:04 -0500

    let instead of var

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 String.prototype.getHashCode = function() {
-  var hash = 0;
+  let hash = 0;
 
   if (this.length === 0) return hash;
 
   for (i = 0; i < this.length; i++) {
-    var char = this.charCodeAt(i);
+    let char = this.charCodeAt(i);
     hash = ((hash << 5) - hash) + char;
     hash = hash & hash;
   }
