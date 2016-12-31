e9ec15608a3caf3607eba7dd8768cda6c3018539
John Hennessey <jghennes@gmail.com>
2016-12-31 12:28:45 -0500

    Move hash strategies to their own folder

---
+++ hashes/java-hash.js
@@ -0,0 +1,15 @@
+function javaHash(string) {
+  let hash = 0;
+
+  if (string.length === 0) return hash;
+
+  for (i = 0; i < string.length; i++) {
+    let char = string.charCodeAt(i);
+    hash = ((hash << 5) - hash) + char;
+    hash = hash & hash;
+  }
+
+  return hash;
+};
+
+module.exports = javaHash;
--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const int_RGB = require('./converters/int_RGB');
+const javaHash = require('./hashes/java-hash');
 const RGB = require('./data-objects/RGB');
 
-String.prototype.getHashCode = function() {
-  let hash = 0;
+String.prototype.getHashCode = function(hashStrategy) {
+    if (hashStrategy) return hashStrategy(this);
 
-  if (this.length === 0) return hash;
-
-  for (i = 0; i < this.length; i++) {
-    let char = this.charCodeAt(i);
-    hash = ((hash << 5) - hash) + char;
-    hash = hash & hash;
-  }
-
-  return hash;
+    return javaHash(this);
 }
 
 Number.prototype.toRGB = function() {
