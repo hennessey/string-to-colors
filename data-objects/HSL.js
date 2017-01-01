754b2674366e11419f705348339c051c6c2d54d1
John Hennessey <jghennes@gmail.com>
2017-01-01 13:44:29 -0500

    Remove prototype methods from data-objects

--- data-objects/HSL.js
+++ data-objects/HSL.js
@@ -1,13 +1,7 @@
-const HSL_RGB = require('../converters/HSL_RGB');
-
 function HSL(hue, sat, light) {
   this.hue = hue,
   this.sat = sat,
   this.light = light
-};
-
-HSL.prototype.toRGB = function () {
-  return HSL_RGB(this);
 };
 
 module.exports = HSL;
--- data-objects/RGB.js
+++ data-objects/RGB.js
@@ -1,18 +1,7 @@
-const RGB_HSL = require('../converters/RGB_HSL');
-const RGB_hex = require('../converters/RGB_hex');
-
 function RGB(red, green, blue) {
     this.red = red;
     this.blue = blue;
     this.green = green;
-};
-
-RGB.prototype.toHSL = function() {
-  return RGB_HSL(this);
-};
-
-RGB.prototype.toHex = function() {
-  return RGB_hex(this);
 };
 
 module.exports = RGB;
